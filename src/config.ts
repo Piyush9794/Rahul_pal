b3a2ecba9d8d142a5e96eb0d2660c3bee605a190
Piyush <vishwakarmapiyush327@gmail.com>
2026-09-13 15:00:17 +0530

    iii

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // src/config.ts
 export const BUSINESS_CONFIG = {
-  whatsappNumber: "8400867766",
+  whatsappNumber: "918400867766",
   // whatsappNumber: "9794125123",
   businessName: "The Printing Hub",
   currency: "INR",
